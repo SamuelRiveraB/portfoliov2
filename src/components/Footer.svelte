dfb35af1ed9545cea8d13fdf5b4c584ff53abc1a
Samuel Rivera <114023422+SamuelRiveraB@users.noreply.github.com>
2023-06-12 20:00:05 -0500

    Fix footer logo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,7 +12,7 @@
 <footer class="flex bottom-0 w-full justify-center items-center bg-white p-10">
     <div class="flex flex-col justify-center items-center gap-3">
         <a href="/">
-            <h2 class="cursor-pointer text-bold text-left text-[30px]">Samuel<span class="text-purple-500">.</span></h2>
+            <h2 class="cursor-pointer font-bold text-left text-[30px]">Samuel<span class="text-purple-500">.</span></h2>
         </a>
         <p class="text-2xl justify-center">
             Resources provided by: <a href="https://www.glazestock.com/" target="_blank" rel="noopener noreferrer" class="underline">Glaze</a>
